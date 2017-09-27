7cf0fb9e27d5476b3f2e2c15f10eee29ff651fd7
brianjohnhaas <brianjohnhaas@gmail.com>
2017-09-27 20:27:42 +0000

    update, incl heatmap and gene violin plots

--- docker/run_docker_ports.sh
+++ docker/run_docker_ports.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -it -v `pwd`/../../:/data -p 9000:80 -p 9001:443 -p 9002:8787 trinityctat/scellcegs2017 $*
+sudo docker run --rm -it -v `pwd`/../../:/data -p 9000:8787 trinityctat/scellcegs2017 $*
